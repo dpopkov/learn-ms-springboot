a210093461225e5059724f40f1464e2ffebd625b
Denis Popkov <pkvdenis@gmail.com>
2022-12-10 23:04:25 +0300

    mssb-multiplication: add skeleton project.

---
+++ multiplication/src/main/java/learn/mssb/mssbmultiplication/MssbMultiplicationApplication.java
@@ -0,0 +1,13 @@
+package learn.mssb.mssbmultiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssbMultiplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssbMultiplicationApplication.class, args);
+    }
+
+}
---
+++ multiplication/src/test/java/learn/mssb/mssbmultiplication/MssbMultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.mssb.mssbmultiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssbMultiplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
