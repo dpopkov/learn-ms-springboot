cd098fc1146d540a4d98b1200860e1bd6df7cd13
Denis Popkov <pkvdenis@gmail.com>
2022-12-16 13:50:29 +0300

    mssb-gamification: add skeleton project.

---
+++ gamification/src/main/java/learn/mssb/mssbgamification/MssbGamificationApplication.java
@@ -0,0 +1,13 @@
+package learn.mssb.mssbgamification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssbGamificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssbGamificationApplication.class, args);
+    }
+
+}
---
+++ gamification/src/test/java/learn/mssb/mssbgamification/MssbGamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.mssb.mssbgamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssbGamificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
